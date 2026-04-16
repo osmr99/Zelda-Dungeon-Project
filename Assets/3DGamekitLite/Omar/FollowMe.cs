546ba4f682f761ee3079c1ee38df53e84dc7378b
osmr99 <omarsantiagomorales@gmail.com>
2026-04-16 02:01:54 -0500

    Progress with doors, moving platforms and crates

---
+++ Assets/3DGamekitLite/Omar/FollowMe.cs
@@ -0,0 +1,21 @@
+#pragma warning disable IDE0051
+using UnityEngine;
+
+public class FollowMe : MonoBehaviour
+{
+    //Transform cube;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //int test = GetComponentIndex();
+        //cube = transform.parent;
+        //transform.position = cube.position;
+        //Debug.Log(test);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
